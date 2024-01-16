d37bf3325e4931cd1dc934f533b89e29022bf55f
TheGregAllison <146021687+TheGregAllison@users.noreply.github.com>
2024-01-16 16:31:32 -0600

    Adjusted Cors

--- index.js
+++ index.js
@@ -32,13 +32,20 @@ app.use(
   cors({
     origin: (origin, callback) => {
       if (!origin) return callback(null, true);
+
+      const allowedOrigins = [
+        'http://localhost:8080',
+        'http://localhost:1234',
+        'https://achievement-3-7--illustrious-salamander-21ccf1.netlify.app',
+      ];
+
       if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
         let message =
           'The CORS policy for this application doesn’t allow access from origin ' +
           origin;
         return callback(new Error(message), false);
       }
+
       return callback(null, true);
     },
   })
